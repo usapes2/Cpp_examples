be5dafa7e8576c4414d76c0b172dceed08068958
=Uladzimir Sapeshka <usapeshka@gmail.com>
2018-09-22 11:04:11 -0500

    FindingUrls /bugs fixes

--- String/FindingURLs.cpp
+++ String/FindingURLs.cpp
@@ -1,16 +1,19 @@
-/* protocol-name: //resource-name */
+/* protocol-name: //resource-name
+ * For simplicity, we'll limit ourselves to being sure that one or more
+ * letters precede the :// separator, and at least one character follows it*/
+
+
 
 #include<iostream>
 #include<algorithm>
 #include<string>
 #include<vector>
+using namespace std;
 
 bool not_url_char(char c)
 {
 	// chars, in addition to alphanumerics, that can appear in a URL
-	static const string url_ch = '~;/?:@=&$-_.+!*(),";
-
-	// see whether c can appear in a URL and return the negative
+	static const string url_ch = "~;/?:@=&$-_.+!*(),";
 	return !(isalnum(c) || find(url_ch.begin(),url_ch.end(),c) != url_ch.end());
 
 }
@@ -18,6 +21,32 @@ bool not_url_char(char c)
 string::const_iterator url_end(string::const_iterator b, string::const_iterator e)
 {
 	return find_if(b,e,not_url_char);
+}
+
+string::const_iterator url_beg(string::const_iterator b, string::const_iterator e)
+{ 
+	static const string sep = "://";
+
+	typedef string::const_iterator iter;
+
+	// i marks where the separator was found
+	iter i = b;
+
+	while ((i = search(i,e,sep.begin(),sep.end())) != e) {
+		if (i != b && i + sep.size() != e) {
+			// beg marks the beginning of the protoco-name
+			iter beg = i;
+			while (beg !=b && isalpha(beg[-1]))
+				--beg;
+
+			// is there at least one appropriate char before and after the separator ?
+			if (beg != i && !not_url_char(i[sep.size()]))
+				return beg;
+		}
+		// the separator we found wasn't part of a URL advance i past this separator
+		i += sep.size();
+	}
+	return e;
 }
 
 vector<string> find_urls(const string& s)
@@ -28,8 +57,8 @@ vector<string> find_urls(const string& s)
 
 	// look trough the entire input
 	while (b != e) {
-		// look for one or more leters followed by ://
 
+		// look for one or more leters followed by ://
 		b = url_beg(b,e);
 
 		// if we found it
